1d58f0b7bdbc63586a05a2be27f26e046ea70cc0
dudhwalekaran <dudhwalekaran@gmail.com>
2025-03-18 15:01:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     /**api: {
       bodyParser: false, // Disable default body parser for file uploads
     },**/
+    output: "export",
     eslint: {
       ignoreDuringBuilds: true, // This will disable ESLint during builds
     },
